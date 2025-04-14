b2307971390ce786fdcc223c6930226549a51048
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-14 01:58:26 -0500

    update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,11 +6,18 @@ import {
   SendMessageCommand,
   SendMessageCommandInput,
 } from '@aws-sdk/client-sqs';
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit, OnModuleDestroy {
   private readonly sqsClient: SQSClient;
+  private isPolling = true;
 
   constructor(private readonly configService: ConfigService) {
     this.sqsClient = new SQSClient({
@@ -23,18 +30,37 @@ export class AppService {
       },
     });
   }
+
+  async onModuleInit() {
+    this.startPolling();
+  }
+
+  async onModuleDestroy() {
+    this.isPolling = false;
+  }
+
+  private async startPolling() {
+    Logger.log('🟢 Iniciando polling de mensajes...');
+    while (this.isPolling) {
+      try {
+        const result = await this.receiveMessage();
+        if (result.count > 0) {
+          Logger.log(`📩 Mensajes recibidos: ${result.count}`);
+        }
+      } catch (error) {
+        Logger.error('Error en el polling de mensajes', error);
+      }
+
+      Logger.log('🔴 Polling detenido.');
+      await new Promise((res) => setTimeout(res, 1200000));
+    }
+    Logger.log('🔴 Polling detenido.');
+  }
+
   async sendMessage() {
     const arraysUsers = [
-      {
-        id: 1,
-        name: 'Carlos',
-        lastname: 'Garcia',
-      },
-      {
-        id: 2,
-        name: 'Carlos',
-        lastname: 'Garcia',
-      },
+      { id: 1, name: 'Carlos', lastname: 'Garcia' },
+      { id: 2, name: 'Carlos', lastname: 'Garcia' },
     ];
     const response = await Promise.all(
       arraysUsers.map((data) => {
@@ -44,7 +70,6 @@ export class AppService {
           MessageBody,
         };
         const command = new SendMessageCommand(params);
-
         return this.sqsClient.send(command);
       }),
     );
@@ -64,17 +89,13 @@ export class AppService {
 
     const command = new ReceiveMessageCommand(params);
     const result = await this.sqsClient.send(command);
-    Logger.verbose({
-      result,
-    });
+
     const processed: any[] = [];
 
     if (result.Messages && result.Messages.length > 0) {
       for (const msg of result.Messages) {
         const { ReceiptHandle, Body } = msg;
-        Logger.debug({
-          msg,
-        });
+        Logger.debug({ msg });
         await this.deleteMessage(ReceiptHandle);
         processed.push({
           data: JSON.parse(Body),
@@ -94,7 +115,6 @@ export class AppService {
       QueueUrl: this.configService.getOrThrow<string>('SQS_QUEUE_URL'),
       ReceiptHandle: receiptHandle,
     };
-
     const command = new DeleteMessageCommand(params);
     await this.sqsClient.send(command);
   }
